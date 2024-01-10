1b54ef77fb5aac38f1dad621315b634933629d43
Xyron Brual <xyronb@gmail.com>
2024-01-09 22:18:56 -0500

    Updated home page logic

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,22 @@
-import { UserButton } from "@clerk/nextjs";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import Image from "next/image";
 
-export default function Home() {
+const Home = async () => {
+  // get the current user
+  const user = await currentUser();
+  if (!user) return null;
+
+  // retrieve the user from mongodb and check if onboarded
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
   return (
     <>
       <h1>Home</h1>
     </>
   );
-}
+};
+
+export default Home;
