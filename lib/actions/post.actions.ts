1e0fe498175d0bf94fa04c9c329b17906f90395a
Xyron Brual <xyronb@gmail.com>
2024-01-12 21:10:48 -0500

    Completed fetchPostById function

--- lib/actions/post.actions.ts
+++ lib/actions/post.actions.ts
@@ -51,3 +51,46 @@ export async function createPost({
     throw new Error(`Failed to create post: ${e.message}`);
   }
 }
+
+// function to retrieve a post given a postId string
+export async function fetchPostById(postId: string) {
+  try {
+    connectToDB();
+
+    // find the post in the database
+    const post = await Post.findById(postId)
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id id name image",
+      }) // populate the author field with the user's mongoid, clerkid, name and PFP
+      .populate({
+        path: "circle",
+        model: Circle,
+        select: "_id name image",
+      }) // populate the circleId field with the circle's mongoid, name and PFP
+      .populate({
+        path: "children", //populate the children field
+        populate: [
+          {
+            path: "author",
+            model: User,
+            select: "_id id name parentId image",
+          },
+          {
+            path: "children", //populate the children field within children
+            model: Post, // the model of the nested children
+            populate: {
+              path: "author",
+              model: User,
+              select: "_id id name parentId image",
+            },
+          },
+        ],
+      })
+      .exec();
+    return post;
+  } catch (e: any) {
+    throw new Error(`Failed to fetch post: ${e.message}`);
+  }
+}
