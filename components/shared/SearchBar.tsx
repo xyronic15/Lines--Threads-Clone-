f94b7110d3ae182fa122428687794c384982c2cd
Xyron Brual <xyronb@gmail.com>
2024-02-21 22:37:22 -0500

    Changed the style of the search bar

--- components/shared/SearchBar.tsx
+++ components/shared/SearchBar.tsx
@@ -24,12 +24,12 @@ const SearchBar = ({ route }: { route: string }) => {
     return () => clearTimeout(handleSearch);
   }, [search, route]);
   return (
-    <div className="flex gap-2 align-middle w-full">
-      <CiSearch size={24} />
+    <div className="flex gap-2 align-middle w-full items-center justify-center">
+      <CiSearch size={24} className="text-white" />
       <Input
         value={search}
         onChange={(e) => setSearch(e.target.value)}
-        className="outline-none focus-visible:ring-0 focus-visible:ring-offset-0"
+        className="text-white outline-none focus-visible:ring-0 focus-visible:ring-offset-0 rounded-none bg-transparent border-b-2 focus:border-b-4 border-x-0 border-t-0 focus:border-slate-600 border-b-slate-800 transition-all duration-200"
       />
     </div>
   );
